f1a27f3c71452ce5960af39c4b0964faf20b224f
Vitor Bigelli <bigellivitor@gmail.com>
2022-12-02 12:27:12 -0300

    feat: update meta tag

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ function App() {
           <meta property="og:title" content="GuessMTG - Wordle" />
           <meta property="og:image" content="https://instagram.fldb1-1.fna.fbcdn.net/v/t51.2885-19/277247889_1325386221205931_6828213806774211948_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fldb1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=KvhNxiOZAvAAX8U1rtv&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfBWnw4x4QeO66fSobVNZVz0Mb7RUt4sp7xJpOWkAPs6Rw&oe=63666559&_nc_sid=6136e7" />
           <meta property="og:description" content="Guess the name of the 'Magic, The Gathering' card of the day" />
-          <meta property="og:url" content="https://guess-mtg-app.herokuapp.com/" />
+          <meta property="og:url" content="https://www.guessmtg.com/" />
           <meta property="og:type" content="website" />
         </Head>
         <div className="header w-100 d-flex justify-content-between align-items-center border-bottom mb-3">
